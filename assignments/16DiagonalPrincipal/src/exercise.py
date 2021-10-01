625b83666c60d8a85bc5afd663fa78da0548b647
A01252512 <a01252512@itesm.mx>
2021-10-01 16:52:07 +0000

    V1

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,6 +1,29 @@
+def matrizDiagonal(matrix):
+    listaDiagonal = []
+    for i in range(len(matrix)):
+        listaDiagonal.append(matrix[i][i])
+    return listaDiagonal
+
 
 def main():
-  
+    
+    matriz = []
+    renglon = []
+
+    renglones = int(input())
+    columnas = int(input())
+
+    if renglones != columnas:
+        print('La matriz no es cuadrada')
+    else:
+        for i in range(renglones):
+            renglon = []
+            for j in range(columnas):
+                dato = int(input())
+                renglon.append(dato)
+            matriz.append(renglon)
+        print(matrizDiagonal(matriz))
+
 
 if __name__=='__main__':
     main()
